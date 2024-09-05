de41a9066d7c234fe49565042f07dc67f7a49bae
abatolyg <anatolyg@gmail.com>
2024-09-05 16:15:26 +0300

    Update open-library-tests.py

--- open-library-tests.py
+++ open-library-tests.py
@@ -1,23 +1,55 @@
-# open_library_tests/
-#     config/
-#         __init__.py
-#         config.py
-#     api/
-#         __init__.py
-#         lists_api.py
-#     pages/
-#         __init__.py
-#         base_page.py
-#         lists_page.py
-#     objects/
-#         __init__.py
-#         list_object.py
-#     tests/
-#         __init__.py
-#         conftest.py
-#         test_lists_api.py
-#     utils/
-#         __init__.py
-#         logger.py
-#     requirements.txt
-#     pytest.ini
+|   .env
+|   .gitignore
+|   open-library-tests.py
+|   output.txt
+|   pytest.ini
+|   README.md
+|   report.html
+|   requirements.txt
+|   secret.key
+|   test-automation.log
+|   __init__.py
+|   
++---api
+|   |   base.py
+|   |   lists.py
+|   |   __init__.py
+|   |   
++---config
+|   |   config.py
+|   |   __init__.py
+|   |   
++---interfaces
+|   |   ilist_interface.py
+|   |   test_strategy_listinterface.py
+|   |             
++---objects
+|   |   list.py
+|   |   list_json_data.py
+|   |   login_json_data.py
+|   |   __init__.py
+|   |            
++---pages
+|   |   base.py
+|   |   lists.py
+|   |   list_details.py
+|   |   login.py
+|   |   __init__.py
+|   |   
++---tests
+|   |   conftest.py
+|   |   test_listsapi.py
+|   |   __init__.py
+|   |   
++---testsdata
+|       create_lists.json
+|       list_strategies.json
+|       login_page_data.json
+|       
++---utils
+|   |   logger.py
+|   |   pwd_manager.py
+|   |   singelton_base.py
+|   |   test_date_source.py
+|   |   __init__.py
+
